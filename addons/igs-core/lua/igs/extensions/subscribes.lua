ecf5e8d924f395ef4e5b3e9f7700822055f92cbe
Maxim <76441887+Replicategit@users.noreply.github.com>
2023-05-26 14:30:36 +0300

    Release 232605.4

--- addons/igs-core/lua/igs/extensions/subscribes.lua
+++ addons/igs-core/lua/igs/extensions/subscribes.lua
@@ -8,7 +8,7 @@ function STORE_ITEM:SetSubGroup(sGroupName, iGroupWeight)
                 pl:SetSubscribe(sGroupName)
             end
         end
-    end):SetMeta("ultima_subs")
+    end):SetMeta("ultima_subs", sGroupName)
 
     self.sub_group = self:Insert(IGS.SUB_GROUPS, sGroupName)
     self.sub_group_weight = iGroupWeight or 0
